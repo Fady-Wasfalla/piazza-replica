ed9e3863ef89b2bdcb883633b928a6df46a3e3c7
@fady.wasfalla <fady.wasfalla@gmail.com>
2021-05-08 17:13:45 +0200

    MQ return response

--- src/main/java/RabbitMQ/ConsumerMQ.java
+++ src/main/java/RabbitMQ/ConsumerMQ.java
@@ -30,7 +30,6 @@ public class ConsumerMQ {
         // One Instance of DAL
         ConcurrentMap<String, Object> dalMap = new ConcurrentHashMap<>();
         dalMap.put("core.commands.UserCommands.UserDAL", UserDAL.class.newInstance());
-
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("localhost");
         Connection connection = factory.newConnection();
@@ -39,36 +38,39 @@ public class ConsumerMQ {
         channel = connection.createChannel();
 
         for (String QUEUE_NAME:queueNames) {
-
-        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-        System.out.println("[*] "+QUEUE_NAME + " [*] Waiting for messages. To exit press CTRL+C");
-
-        consumer = new DefaultConsumer(channel) {
-            @Override
-            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body)
-                    throws IOException {
-                Runnable task = new Runnable() {
-                    public void run() {
-                        try {
-                            String message = new String(body, "UTF-8");
-                            JSONObject requestJson = new JSONObject(message);
-                            String function = requestJson.getString("function");
-                            String serviceDAL = requestJson.getString("service");
-                            CommandDP command = (CommandDP) CommandsMap.queryClass(function).newInstance();
-                            Class service = command.getClass();
-                            Method setData = service.getMethod("setData",JSONObject.class, Object.class);
-                            setData.invoke(command, requestJson, dalMap.get("core.commands."+serviceDAL+"Commands."+serviceDAL+"DAL"));
-                            command.execute();
-                            channel.basicAck(envelope.getDeliveryTag(), false);
-                        } catch (Exception e) {
-                            e.printStackTrace();
+            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+            System.out.println("[*] "+QUEUE_NAME + " [*] Waiting for messages. To exit press CTRL+C");
+            consumer = new DefaultConsumer(channel) {
+                @Override
+                public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body)
+                        throws IOException {
+                    Runnable task = new Runnable() {
+                        public void run() {
+                            try {
+                                String message = new String(body, "UTF-8");
+                                JSONObject requestJson = new JSONObject(message);
+                                String function = requestJson.getString("function");
+                                String serviceDAL = requestJson.getString("service");
+                                CommandDP command = (CommandDP) CommandsMap.queryClass(function).newInstance();
+                                Class service = command.getClass();
+                                Method setData = service.getMethod("setData",JSONObject.class, Object.class);
+                                setData.invoke(command, requestJson, dalMap.get("core.commands."+serviceDAL+"Commands."+serviceDAL+"DAL"));
+                                JSONObject result = command.execute();
+                                channel.basicAck(envelope.getDeliveryTag(), false);
+                                AMQP.BasicProperties replyProps = new AMQP.BasicProperties
+                                        .Builder()
+                                        .correlationId(properties.getCorrelationId())
+                                        .build();
+                                channel.basicPublish("", properties.getReplyTo(), replyProps, result.toString().getBytes("UTF-8"));
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
                         }
-                    }
-                };
-                threadPool.submit(task);
-            }
-        };
-        channel.basicConsume(QUEUE_NAME, false, consumer);
+                    };
+                    threadPool.submit(task);
+                }
+            };
+            channel.basicConsume(QUEUE_NAME, false, consumer);
         }
     }
 }
