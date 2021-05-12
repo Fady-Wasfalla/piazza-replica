eda68c108d3c6c021fb035caab31e796f97b6a29
MinaMamdouh2 <40996073+MinaMamdouh2@users.noreply.github.com>
2021-05-12 13:02:51 +0200

    Added Redis into mongoDB data access layer

--- src/main/java/Services/mongoDB.java
+++ src/main/java/Services/mongoDB.java
@@ -4,62 +4,103 @@ import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.FindOneAndUpdateOptions;
 import com.mongodb.client.model.UpdateOptions;
 import org.bson.Document;
 import org.bson.conversions.Bson;
+import org.bson.json.JsonObject;
 import org.bson.types.ObjectId;
 import static com.mongodb.client.model.Updates.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Random;
+
+import java.util.*;
+
 
 public class mongoDB {
-
+    public static MongoClient createMongoClient(String connectionString){
+        try (MongoClient mongoClient = MongoClients.create(connectionString)) {
+            return mongoClient;
+        }
+    }
     public static MongoCollection<Document> getCollection(MongoClient mongoClient, String databaseName,
                                                           String collectionName) {
         MongoDatabase database = mongoClient.getDatabase(databaseName);
         return database.getCollection(collectionName);
     }
 
-    public static void create(MongoClient mongoClient, String databaseName, String collectionName,
-                              Document document) {
-        MongoCollection<Document> collection = getCollection(mongoClient, databaseName, collectionName);
+    public static void create( MongoClient mongoClient, String databaseName, String collectionName, Document document,
+                               jedis jedis, String key) {
+        MongoCollection<Document> collection = getCollection( mongoClient, databaseName, collectionName);
         collection.insertOne(document);
+        jedis.deleteCache(collectionName);
+        jedis.setLayeredCache(collectionName + document.get(key).toString(), key ,(document.toJson()).toString());
     }
 
-    public static ArrayList read(MongoClient mongoClient, String databaseName, String collectionName,
-                                 Document filterDocument) {
+    public static Document readOne(MongoClient mongoClient, String databaseName, String collectionName,
+                                 Document filterDocument, jedis jedis, String key) {
+        String cached = jedis.getLayeredCache(collectionName + filterDocument.get(key).toString(), filterDocument.toString());
+        if (cached != null)
+            return Document.parse(cached);
         MongoCollection<Document> collection = getCollection(mongoClient, databaseName, collectionName);
-        return collection.find(filterDocument).into(new ArrayList<>());
+        Document document = collection.find(filterDocument).first();
+        if (document != null){
+            jedis.setLayeredCache(collectionName + filterDocument.get(key).toString(), filterDocument.toString(), (document.toJson()).toString());
+
+        return document;
+               }
+        return new Document();
     }
 
-    public static void update(MongoClient mongoClient, String databaseName, String collectionName,
-                              Document filterDocument, Bson updateOperation, UpdateOptions options) {
+    public static Document update( MongoClient mongoClient, String databaseName, String collectionName,
+                              Document filterDocument, Bson updateOperation, FindOneAndUpdateOptions options,
+                                   jedis jedis, String key) {
         MongoCollection<Document> collection = getCollection(mongoClient, databaseName, collectionName);
-        collection.updateOne(filterDocument, updateOperation, options);
+        Document document =  collection.findOneAndUpdate(filterDocument, updateOperation, options);
+        jedis.deleteCache(collectionName);
+        jedis.deleteCache(collectionName + document.get(key).toString());
+        jedis.setLayeredCache(collectionName + filterDocument.get(key).toString(), filterDocument.toString(),
+                (document.toJson()).toString());
+        return document;
     }
 
-    public static void updateMany(MongoClient mongoClient, String databaseName, String collectionName,
-                                  Document filterDocument, Bson updateOperation, UpdateOptions options) {
-        MongoCollection<Document> collection = getCollection(mongoClient, databaseName, collectionName);
+    public static void updateMany(MongoClient mongoClient , String databaseName, String collectionName,
+                                  Document filterDocument, Bson updateOperation, UpdateOptions options, jedis jedis) {
+        MongoCollection<Document> collection = getCollection( mongoClient, databaseName, collectionName);
+        Set<String> cacheKeys = jedis.returnKeys(collectionName + "*");
+        Iterator<String> cacheKeysIterator = cacheKeys.iterator();
+        while(cacheKeysIterator.hasNext())
+            jedis.deleteCache(cacheKeysIterator.next());
         collection.updateMany(filterDocument, updateOperation, options);
     }
 
-    public static void deleteOne(MongoClient mongoClient, String databaseName, String collectionName,
-                                 Document filterDocument) {
-        MongoCollection<Document> collection = getCollection(mongoClient, databaseName, collectionName);
-        collection.deleteOne(filterDocument);
+    public static Document deleteOne(MongoClient mongoClient, String databaseName, String collectionName,
+                                 Document filterDocument, jedis jedis, String key) {
+        MongoCollection<Document> collection = getCollection( mongoClient, databaseName, collectionName);
+        Document document = collection.findOneAndDelete(filterDocument);
+        if(document != null) {
+            jedis.deleteCache(collectionName);
+            jedis.deleteCache(collectionName + document.get(key).toString());
+            return document;
+        }
+        return  new Document();
     }
 
     public static void deleteMany(MongoClient mongoClient, String databaseName, String collectionName,
-                                  Document filterDocument) {
-        MongoCollection<Document> collection = getCollection(mongoClient, databaseName, collectionName);
+                                  Document filterDocument, jedis jedis) {
+        MongoCollection<Document> collection = getCollection( mongoClient, databaseName, collectionName);
+        Set<String> cacheKeys = jedis.returnKeys(collectionName + "*");
+        Iterator<String> cacheKeysIterator = cacheKeys.iterator();
+        while(cacheKeysIterator.hasNext())
+            jedis.deleteCache(cacheKeysIterator.next());
         collection.deleteMany(filterDocument);
+
     }
 
     public static void main(String[] args) {
         String connectionString = "mongodb+srv://admin:admin@cluster0.rcrnf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-        try (MongoClient mongoClient = MongoClients.create(connectionString)) {
+        jedis jedis = new jedis("localhost", 6379, "");
+        //check whether server is running or not
+        try (MongoClient mongo_client = MongoClients.create(connectionString)) {
+            System.out.println("Connection to server sucessfully");
             Random rand = new Random();
             Document student = new Document("_id", new ObjectId());
             student.append("student_id", 10002d)
@@ -82,19 +123,22 @@ public class mongoDB {
             //           { type: "homework",
             //             score: 27 }
             //                          ]
-            create(mongoClient,"sample_training", "grades", student);
-            // returns all instances with student_id > 1002
-            ArrayList x = read(mongoClient,"sample_training", "grades",
-                  new Document("student_id", new Document("$gte",10002)));
-
+//            create(mongo_client, "sample_training", "grades", student , jedis, "_id");
+//             returns all instances with student_id > 1002
+//            Document x = readOne(mongo_client, "sample_training", "grades",
+//                    new Document("student_id", new Document("$gte", 10000)), jedis, "student_id");
             // updates the first instance of student_id: 10001
             // update options like set ex: Bson updateOperation = set("comment", "You should learn MongoDB!");
-
-           update(mongoClient,"sample_training", "grades", new Document("student_id",10001),
-                  set("message", "testing"), new UpdateOptions());
-           // delete all instances with student_id > 1001
-             deleteMany(mongoClient,"sample_training", "grades",
-                 new Document("student_id", new Document("$gte",10001)));
+//
+//            Document updated_document = update(mongo_client, "sample_training", "grades", new Document("student_id", 10002),
+//                    set("message", "testing"), new FindOneAndUpdateOptions());
+//            System.out.println(updated_document);
+            // delete all instances with student_id > 1001
+//            Document deleted_document = deleteOne(mongo_client, "sample_training", "grades",
+//                    new Document("student_id", new Document("$gte", 10000)), jedis, "student_id");
+//            System.out.println(deleted_document);
+//            deleteMany(mongo_client, "sample_training", "grades",
+//                    new Document("student_id", new Document("$gte", 10000)), jedis);
         }
     }
 }
