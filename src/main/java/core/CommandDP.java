326becd1ffb4786835590492fb08d28ebb3a05b9
RamyGabra <ramygabra5@gmail.com>
2021-05-13 01:26:37 +0000

    Adding schema validation

--- src/main/java/core/CommandDP.java
+++ src/main/java/core/CommandDP.java
@@ -1,8 +1,12 @@
 package core;
 
-import com.mongodb.client.MongoClient;
 
+import com.mongodb.client.MongoClient;
 import org.json.JSONObject;
+import javax.xml.validation.Schema;
+import java.util.ArrayList;
+import java.util.Iterator;
+
 
 public abstract class CommandDP {
     public JSONObject data;
@@ -12,5 +16,19 @@ public abstract class CommandDP {
     public void setData(JSONObject data, MongoClient mongoClient ){
         this.data = data;
         this.mongoClient=mongoClient;
+    }
+
+
+
+    public boolean validateJSON(ArrayList<String> schema, JSONObject jsonData){
+
+        for(int i =0; i < schema.size(); i++) {
+            String key = schema.get(i);
+            if(!jsonData.has(key)){
+                return false;
+            }
+        }
+        
+        return true;
     }
 }
