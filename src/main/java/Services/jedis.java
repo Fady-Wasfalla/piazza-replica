ff4c63de0c071beee13a8b78da827a37c8e2faa7
MinaMamdouh2 <40996073+MinaMamdouh2@users.noreply.github.com>
2021-05-14 17:31:49 +0200

    Added PostgrSQL layer

---
+++ src/main/java/Services/jedis.java
@@ -0,0 +1,30 @@
+package Services;
+import redis.clients.jedis.Jedis;
+
+import java.util.Set;
+
+public class jedis {
+    Jedis jedis;
+    public jedis(String host, int port, String password){
+        Jedis jedis = new Jedis(host, port);
+        if(password != "")
+            jedis.auth(password);
+        this.jedis = jedis;
+    }
+
+    public void setLayeredCache( String key, String query, String cachedDocument){
+        this.jedis.hset(key, query, cachedDocument);
+    }
+    public String getLayeredCache( String key, String query){
+        return this.jedis.hget(key,query);
+    }
+    public void deleteCache(String key){
+        this.jedis.del(key);
+    }
+    public Set<String> returnKeys(String key){
+        return jedis.keys(key);
+    }
+    public Jedis getJedisClient(){
+        return this.jedis;
+    }
+}
